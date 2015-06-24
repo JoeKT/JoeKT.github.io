72375c84c4d9e83e8353da80c4ca0e8b6f6d1378
JKosciuskoTritschler <jrtritschler@gmail.com>
2015-06-24 17:57:46 -0600

    First Push

---
+++ js/hover.js
@@ -0,0 +1,13 @@
+// $('.resume-sketch').hover(fadeOut, fadeIn);
+
+// var fadeOut = function() {
+
+// 	$('.resume-sketch').css('display', 'none');
+// 	$('.resume-hover').css('display', 'block');
+// };
+
+// var fadeIn = function () {
+
+// 	$('.resume-hover').css('display', 'none');
+// 	$('.resume-sketch').css('display', 'block');
+// };
