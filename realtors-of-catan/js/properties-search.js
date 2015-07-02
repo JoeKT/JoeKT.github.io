af5c7e9a5537a5887aeddfb4c4393af4c64e966d
JKosciuskoTritschler <jrtritschler@gmail.com>
2015-07-02 12:31:55 -0600

    Slight Adjustments

--- realtors-of-catan/js/properties-search.js
+++ realtors-of-catan/js/properties-search.js
@@ -2,24 +2,24 @@ var properties = function () {
 	
 	$('#allSales').click(function() {
 
-		$('.fa-home, .fa-usd, .fa-university').css('visibility', 'visible');
+		$('.home-sale, .fa-usd, .fa-university').css('visibility', 'visible');
 	});
 
 	$('#undevelopedLand').click(function () {
 
-		$('.fa-home, .fa-university').css('visibility', 'hidden');
+		$('.home-sale, .fa-university').css('visibility', 'hidden');
 		$('.fa-usd').css('visibility', 'visible');
 	});
 
 	$('#smallProperties').click(function () {
 
 		$('.fa-university, .fa-usd').css('visibility', 'hidden');
-		$('.fa-home').css('visibility', 'visible');
+		$('.home-sale').css('visibility', 'visible');
 	});
 
 	$('#largeProperties').click(function () {
 
-		$('.fa-home, .fa-usd').css('visibility', 'hidden');
+		$('.home-sale, .fa-usd').css('visibility', 'hidden');
 		$('.fa-university').css('visibility', 'visible');
 	});
 };
